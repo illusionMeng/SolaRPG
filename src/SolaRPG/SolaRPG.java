6f3bc1147494f0db40f46e69bb8471c186e86d31
illusionMeng <mengcheang511@gmail.com>
2018-03-30 15:54:21 +0100

    Initial commit

---
+++ src/SolaRPG/SolaRPG.java
@@ -0,0 +1,14 @@
+package SolaRPG;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SolaRPG extends JavaPlugin {
+	
+	public void onEnable() {
+		getServer().getConsoleSender().sendMessage("[SolaRPG] SolaRPG Enabled!");
+	}
+	
+	public void onDisable() {
+		
+	}
+}
