177471fe02eab30f81b93370ce68384bf3522f7b
Meng <mengcheang511@gmail.com>
2018-08-16 22:21:29 +0800

    v0.0.5 again
    marcus dllm

--- src/SolaRPG/SolaRPG.java
+++ src/SolaRPG/SolaRPG.java
@@ -1,8 +1,16 @@
 package SolaRPG;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SolaRPG extends JavaPlugin {
@@ -12,6 +20,21 @@ public class SolaRPG extends JavaPlugin {
 		defaultConfig();
 		getServer().getPluginManager().registerEvents(new Event(this), this);
 		getServer().getConsoleSender().sendMessage("[SolaRPG] SolaRPG Enabled!");
+		
+		ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+		NamespacedKey key;
+	    ItemMeta test01 = items.get(0).getItemMeta();
+	    test01.setDisplayName(ChatColor.GOLD + "破邪之劍");
+	    test01.setLore(Arrays.asList(new String[] {
+	       (ChatColor.WHITE + "擁有強大力量的劍") , 
+	       (ChatColor.RED + "非常危險!")
+	    }));
+	    items.get(0).setItemMeta(test01);
+		key = new NamespacedKey(this , "item01");
+	    ShapedRecipe test1 = new ShapedRecipe(key, items.get(0)).shape("   ","!/!","   ")
+	    .setIngredient('!',Material.BEDROCK)
+	    .setIngredient('/',Material.GOLD_BLOCK);
+	    getServer().addRecipe(test1);
 	}
 	
 	@Override
